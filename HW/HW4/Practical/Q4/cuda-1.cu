0814b426c2c8994ab3f7e64c6a0996eb1a1dd133
Amirmahdi Namjoo <amirm137878@gmail.com>
2022-07-26 05:56:40 +0430

    Update HW4 Q4
    Add time to cuda codes
    Better Memory handling in cuda codes
    Add Makefile
    
    Co-Authored-By: Saba Hashemi <45356628+Savaw@users.noreply.github.com>

--- HW/HW4/Practical/Q4/cuda-1.cu
+++ HW/HW4/Practical/Q4/cuda-1.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "bmp.c"
-#include <assert.h> 
+#include <assert.h>
 
 #define DATA_OFFSET_OFFSET 0x000A
 #define WIDTH_OFFSET 0x0012
@@ -20,93 +20,102 @@ typedef unsigned char byte;
 
 __device__
 int is_green(byte r, byte g, byte b) {
-  int green_dist = (byte) 255 + r + b,
-      red_dist = (byte) 255 + g + b,
-      blue_dist = (byte) 255 + r + g;
-  return (green_dist < red_dist && green_dist < blue_dist);
+    int green_dist = (byte) 255 + r + b,
+            red_dist = (byte) 255 + g + b,
+            blue_dist = (byte) 255 + r + g;
+    return (green_dist < red_dist && green_dist < blue_dist);
 }
 
 __global__
-void replace(byte *pixels_top, 
-            byte *pixels_bg, 
-            int width, 
-            int height,
-            int bytesPerPixel) {
+void replace(byte *pixels_top,
+             byte *pixels_bg,
+             int width,
+             int height,
+             int bytesPerPixel) {
 
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      int start = i * width + j;
-      if (is_green(pixels_top[start * bytesPerPixel],
-                   pixels_top[start * bytesPerPixel + 1],
-                   pixels_top[start * bytesPerPixel + 2])) {
-        pixels_top[start * bytesPerPixel] = pixels_bg[start * bytesPerPixel];
-        pixels_top[start * bytesPerPixel + 1] = pixels_bg[start * bytesPerPixel + 1];
-        pixels_top[start * bytesPerPixel + 2] = pixels_bg[start * bytesPerPixel + 2];
-      }
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int start = i * width + j;
+            if (is_green(pixels_top[start * bytesPerPixel],
+                         pixels_top[start * bytesPerPixel + 1],
+                         pixels_top[start * bytesPerPixel + 2])) {
+                pixels_top[start * bytesPerPixel] = pixels_bg[start * bytesPerPixel];
+                pixels_top[start * bytesPerPixel + 1] = pixels_bg[start * bytesPerPixel + 1];
+                pixels_top[start * bytesPerPixel + 2] = pixels_bg[start * bytesPerPixel + 2];
+            }
+        }
     }
-  }
 
 }
 
-int main()
-{
-  /* start reading the file and its information*/
-  byte *pixels_top, *pixels_bg;
-  int32 width_top, width_bg;
-  int32 height_top, height_bg;
-  int32 bytesPerPixel_top, bytesPerPixel_bg;
-  ReadImage("dino.bmp", &pixels_top, &width_top, &height_top, &bytesPerPixel_top);
-  ReadImage("parking.bmp", &pixels_bg, &width_bg, &height_bg, &bytesPerPixel_bg);
+int main() {
+    cudaEvent_t start, stop;
+    float ms = 0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    /* start reading the file and its information*/
+    byte *pixels_top, *pixels_bg;
+    int32 width_top, width_bg;
+    int32 height_top, height_bg;
+    int32 bytesPerPixel_top, bytesPerPixel_bg;
+    ReadImage("dino.bmp", &pixels_top, &width_top, &height_top, &bytesPerPixel_top);
+    ReadImage("parking.bmp", &pixels_bg, &width_bg, &height_bg, &bytesPerPixel_bg);
 
-  /* images should have color and be of the same size */
-  assert(bytesPerPixel_top == 3);
-  assert(width_top == width_bg);
-  assert(height_top == height_bg); 
-  assert(bytesPerPixel_top == bytesPerPixel_bg); 
-
-
-  /* we can now work with one size */
-  int32 width = width_top, height = height_top, bytesPerPixel = bytesPerPixel_top; 
-
-  size_t total_size_bytes = width * height * bytesPerPixel;
+    /* images should have color and be of the same size */
+    assert(bytesPerPixel_top == 3);
+    assert(width_top == width_bg);
+    assert(height_top == height_bg);
+    assert(bytesPerPixel_top == bytesPerPixel_bg);
 
 
-  /* Malloc memory on device */
-  byte *pixels_top_device, *pixels_bg_device;
+    /* we can now work with one size */
+    int32 width = width_top, height = height_top, bytesPerPixel = bytesPerPixel_top;
 
-  cudaMalloc(&pixels_top_device, total_size_bytes);
-  cudaMalloc(&pixels_bg_device, total_size_bytes);
-
-
-  /* Move images to device */
-  cudaMemcpy(pixels_top_device, pixels_top, total_size_bytes, cudaMemcpyHostToDevice);
-  cudaMemcpy(pixels_bg_device, pixels_bg, total_size_bytes, cudaMemcpyHostToDevice);
+    size_t total_size_bytes = width * height * bytesPerPixel;
 
 
-  /* start replacing green screen */
-  int blockSize = 1;
-  int numBlocks = 1;
-  replace<<<blockSize,numBlocks>>>(pixels_top_device, 
-                  pixels_bg_device,
-                  width, 
-                  height, 
-                  bytesPerPixel);
+    /* Malloc memory on device */
+    byte *pixels_top_device, *pixels_bg_device;
 
-  cudaDeviceSynchronize();
-  
+    cudaMalloc(&pixels_top_device, total_size_bytes);
+    cudaMalloc(&pixels_bg_device, total_size_bytes);
 
-  /* Move result to host */
-  cudaMemcpy(pixels_top, pixels_top_device, total_size_bytes, cudaMemcpyDeviceToHost);
- 
 
-  /* write new image */
-  WriteImage("replaced.bmp", pixels_top, width, height, bytesPerPixel);
-  
-  /* free everything */
-  free(pixels_top);
-  free(pixels_bg);
-  cudaFree(pixels_top_device);
-  cudaFree(pixels_bg_device);
+    /* Move images to device */
+    cudaMemcpy(pixels_top_device, pixels_top, total_size_bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(pixels_bg_device, pixels_bg, total_size_bytes, cudaMemcpyHostToDevice);
 
-  return 0;
+
+    /* start replacing green screen */
+    int blockSize = 1;
+    int numBlocks = 1;
+    cudaEventRecord(start);
+    replace<<<blockSize, numBlocks>>>(pixels_top_device,
+                                      pixels_bg_device,
+                                      width,
+                                      height,
+                                      bytesPerPixel);
+
+    cudaEventRecord(stop);
+    cudaDeviceSynchronize();
+
+
+    cudaEventElapsedTime(&ms, start, stop);
+    printf("Time: %f ms\n", ms);
+
+    cudaMallocHost(&pixels_top, total_size_bytes);
+    /* Move result to host */
+    cudaMemcpy(pixels_top, pixels_top_device, total_size_bytes, cudaMemcpyDeviceToHost);
+
+
+    /* write new image */
+    WriteImage("replaced.bmp", pixels_top, width, height, bytesPerPixel);
+
+    /* free everything */
+    cudaFree(pixels_top);
+    free(pixels_bg);
+    cudaFree(pixels_top_device);
+    cudaFree(pixels_bg_device);
+
+    return 0;
 }
